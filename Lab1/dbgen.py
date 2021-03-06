524813a79b047b2fede98ba4efe0e7259100aef8
Marcel Cases i Freixenet <m@rcel.xyz>
2021-03-06 19:00:41 +0100

    Lab1 base datagen created
    Signed-off-by: Marcel Cases i Freixenet <m@rcel.xyz>

---
+++ Lab1/dbgen.py
@@ -0,0 +1,23 @@
+# Script to generate CPU loads randomly, for simulation purposes
+# m@rcel.xyz
+
+import random
+
+nTasks = 50
+nCPUs = 70
+rt = []
+rc = []
+
+for i in range(0,nTasks):
+    rt.append(round(random.uniform(100, 700),2))
+
+for i in range(0,nCPUs):
+    rc.append(round(random.uniform(100, 700),2))
+
+str_rt = str(rt).replace(',', '')
+str_rc = str(rc).replace(',', '')
+
+print("nTasks =", nTasks, ";")
+print("nCPUs =", nCPUs, ";")
+print("rt =", str_rt, ";")
+print("rc =", str_rc, ";")
